763bccbe194015b45e0d061425c15234211559df
Alfrazgit <ahmedalfraz30gmail.com>
2024-03-20 21:51:54 +0530

    server/index.js

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ io.on("connect", (socket) => {
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit("message", {
       user: "admin",
       text: `${user.name}, welcome to room ${user.room}.`,
@@ -27,8 +29,6 @@ io.on("connect", (socket) => {
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", text: `${user.name} has joined!` });
-
-    socket.join(user.room);
 
     io.to(user.room).emit("roomData", {
       room: user.room,
@@ -41,12 +41,7 @@ io.on("connect", (socket) => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
-    console.log(user, message);
     io.to(user.room).emit("message", { user: user.name, text: message });
-    io.to(user.room).emit("roomData", {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
 
     callback();
   });
